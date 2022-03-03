99a72cf148d7050638ea2d5f144bed03e209ce1e
ibCom <hello@ibcom.biz>
2022-03-04 08:50:37 +1100

    monitoring

--- 1blankspace.control-3.9.17.js
+++ 1blankspace.control-3.9.17.js
@@ -468,7 +468,7 @@ ns1blankspace.scripts =
 	},
 	{
 		nameSpace: '1blankspace.admin.monitoring',
-		source: '/site/1903/1blankspace.admin.monitoring-3.6.4.js'
+		source: '/site/1903/1blankspace.admin.monitoring-3.9.17.js'
 	},
 	{
 		nameSpace: '1blankspace.admin.schema',
